15fb8f2470a642a29d24cb86c6fb3206abb64f06
kumar-manish000004 <124406146+kumar-manish000004@users.noreply.github.com>
2023-02-04 06:04:32 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,7 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href=""
+          href="https://github.com/"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
